80404e990793ed6c5db617e9635caca4c3969ae7
Cole Miller <cole.t.miller@gmail.com>
2022-06-01 18:18:24 -0500

    coming soon title in card

--- pages/coming-soon.js
+++ pages/coming-soon.js
@@ -92,9 +92,10 @@ const ComingSoon = ({ ids }) => {
 
         <Waves fillColor="#fedf87" />
         <div className="fiendz-card-container w-50 md:w-7/12 lg:m-3 relative justify-center mt-16 flex flex-col bg-white items-center pt-10 lg:w-8/12">
-          <div className="w-48 md:w-72 lg:w-80 absolute -top-12 md:-top-16 coming-soon-title">
-            <Image src={logoBig} alt="Main Fantom Alpha Fiendz Logo" />
-          </div>
+          <h1 className="font-freckle text-4xl md:text-6xl main-title-text">
+            Fantom{" "}
+            <span className="main-title-text-secondary">Alpha Fiendz</span>
+          </h1>
           <div className="flex flex-col font-inter content-line text-base sm:text-lg md:text-xl font-normal text-center w-full my-4 md:my-10 xl:mt-18 items-center px-4">
             <h1 className="font-freckle text-border text-xl md:text-2xl md:text-4xl lg:text-6xl">
               <b>MINT IS COMING SOON</b>
